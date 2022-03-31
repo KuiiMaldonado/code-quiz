832f83d997581b0871ca5e690ff68efb98d4721e
unknown <cuitlahuacjr@gmail.com>
2022-03-30 20:35:20 -0600

    All questions and messages show correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,49 @@
 //Array with questions
-const questions = [{
+const questions = [
+    {
         question: 'Commonly used data types DO NOT include:',
         options: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 2
+        answer: 3
     },
     {
         question: 'The condition in and if / else statement is enclosed within ________.',
         options: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-        answer: 2
+        answer: 3
     },
     {
         question: 'Arrays in JavaScript can be used to store ______.',
         options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 3
+        answer: 4
     },
     {
         question: 'String values must be enclosed within _______ when being assigned to variables.',
         options: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-        answer: 2
+        answer: 3
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
         options: ['JavaScript', 'termina/bash', 'for loops', 'console.log'],
-        answer: 3
+        answer: 4
     }
 ];
 
 //Top right countdown timer
 const timer = document.getElementById('timer');
 const questionContainer = document.getElementById('question-container');
+const scoreContainer = document.getElementById('score-container');
+const submit = document.getElementById('submit');
 var activeQuestion = undefined;
+var timeInterval;
+var timeLeft;
 
 //Function that will trigger the timer
 function countdown() {
 
-    var timeLeft = 75;
-    var timeInterval = setInterval(function () {
+    timeLeft = 75;
+    timeInterval = setInterval(function () {
 
-        timer.textContent = 'Time: ' + timeLeft;
         timeLeft--;
+        timer.textContent = 'Time: ' + timeLeft;
         if (timeLeft < 0) {
             clearInterval(timeInterval);
         }
@@ -54,10 +59,10 @@ function cleanQuestionContainer() {
 //Function that will create and append the question elements to the DOM
 //depending on the parameter it will create the template for another question
 //or the score template
-//@params - 0 creates the score template, 1 creates question template
+//@params - 0 creates question template, 1 creates initials template, 2 creates high-scores template
 function createTemplateElements(template) {
 
-    if (template === 1) {
+    if (template === 0) {
         let questionText = document.createElement('h4');
         let answersList = document.createElement('ul');
 
@@ -76,8 +81,33 @@ function createTemplateElements(template) {
             liElement.appendChild(buttonElement);
         }
     }
-    else {
+    else if (template === 1){
+        questionContainer.setAttribute('class', "hidden");
+        scoreContainer.classList.remove('hidden');
+        let finalScore = document.getElementById('final-score');
+        finalScore.textContent = 'Your final score is ' + timeLeft;
+    }
+    else if (template === 2) {
+        let header = document.getElementById('score-header');
+        let text = document.getElementById('final-score');
+        let form = document.getElementById('form');
 
+        let player = localStorage.key(0);
+
+        header.textContent = 'High Scores';
+        text.textContent = '1. ' + player + '-' + localStorage.getItem(player);
+        form.remove();
+
+        let backButton = document.createElement('button');
+        let clearScores = document.createElement('button');
+
+        backButton.setAttribute('id', 'back-button');
+        clearScores.setAttribute('id', 'clear-scores');
+        backButton.textContent = 'Back';
+        clearScores.textContent = 'Clear scores';
+
+        scoreContainer.appendChild(backButton);
+        scoreContainer.appendChild(clearScores);
     }
 }
 
@@ -94,27 +124,39 @@ function renderQuestion() {
     }
 }
 
-//Function to handle the clicks of the click
-function clickHandler(event) {
+//Function to handle the clicks of the questionContainer
+function questionContainerClickHandler(event) {
 
     let element = event.target;
 
     if (element.matches('#startQuiz')) {
         cleanQuestionContainer();
         activeQuestion = 0;
-        createTemplateElements(1);
+        createTemplateElements(0);
         countdown();
         renderQuestion();
     }
-    else {
+    else{
         activeQuestion++;
         if (activeQuestion < questions.length)
             renderQuestion();
         else {
-
+            clearInterval(timeInterval);
+            cleanQuestionContainer();
+            createTemplateElements(1);
         }
     }
 }
 
-//Event listener for startQuiz button
-questionContainer.addEventListener('click', clickHandler);
+//Function to submit score
+function submitScore(event) {
+
+    event.preventDefault();
+    let inputText = document.getElementById('initials');
+    localStorage.setItem(inputText.value, timeLeft);
+    createTemplateElements(2);
+}
+
+//Event listener for clicks in question-container element
+questionContainer.addEventListener('click', questionContainerClickHandler);
+submit.addEventListener('click', submitScore);
